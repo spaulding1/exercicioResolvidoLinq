f48ab350b54391555017d86c89cb9e3105b09de9
Anderson Uchoa <andersonuchoa3@gmail.com>
2021-07-11 23:08:19 -0300

    exemplo com linq

---
+++ Entities/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeResolvLinq.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using ExeResolvLinq.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace ExeResolvLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PEGAR O CAMINHO DO ARQUIVO .TXT
+            Console.Write("Digite o caminho completo do arquivo: ");
+            string path = Console.ReadLine();
+
+            List<Produto> list = new List<Produto>();
+
+            //LER O ARQUIVO E ADICIONAR OS ITENS EM UMA LISTA
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] campos = sr.ReadLine().Split(',');
+                    string nome = campos[0];
+                    double preco = double.Parse(campos[1],CultureInfo.InvariantCulture);
+                    list.Add(new Produto(nome, preco));
+                }
+            }
+
+            //CALCULAR PREÇO MÉDIO
+            var avg = list.Select(p => p.Preco).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Média de preço = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            //LISTA DE NOMES DE PRODUTOS EM ORDEM DESCRESCENTE COM VALOR ABAIXO DA MÉDIA
+            var nomes = list.Where(p => p.Preco < avg).OrderByDescending(p => p.Nome).Select(p => p.Nome);
+            foreach (var item in nomes)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
